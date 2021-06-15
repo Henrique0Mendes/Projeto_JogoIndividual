b6795429856e7850d70adeca32410684cbd20ef0
Henrique0Mendes <henriquemendes1711@gmail.com>
2021-06-15 22:53:08 +0100

    Modal login | Criar personagem

--- jogo/src/app/app-routing.module.ts
+++ jogo/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Host, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarComponent } from './components/criar/criar.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"Home",component:HomeComponent},
+  {path:"CriarPersonagem",component:CriarComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jogo/src/app/components/home/home.component.ts
+++ jogo/src/app/components/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
+
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+
+  open(content) {
+    this.modalService.open(content);
+  }
 
   ngOnInit(): void {
   }
 
 }
+
